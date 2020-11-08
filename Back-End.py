01389f6d8fdf38b426f45c3e36324b3d9e967160
Tonymoka <mhd.smith94@gmail.com>
2020-11-07 18:10:24 -0600

    Co-authored-by: razortyphon@gmail.com <razortyphon@gmail.com> Co-authored-by: Sonny Ha <sonnyha@live.com> Co-authored-by: mwa65867@gmail.com <mwa65867@gmail.com>

--- Back-End.py
+++ Back-End.py
@@ -1,12 +1,27 @@
-import numpy as np
-import math
+import numpy as np #not used yet
+from matplotlib import pyplot as plt #plot the histogram
 
-major_dic= {"communication": 45,
+
+#get more accurate salaries for better output
+major_dic= {"Communication": 45,
             "Business": 50, "STEM": 65,
             "Education ": 45, "MD":250,
-            "MBA": 100, "Liberal Arts": 40}
+            "MBA": 100, "Liberal Arts": 40,
+            "Law" : 95} #Major Inforation
+#3 var... salary, debt, interst rate, year of graduation (depending on what the user puts in) 
+#Histogram name = the key in dictionary
+#var which equal salary to major * increase
+#var which equal to debt * intreset
+ages = [10,20]# sample 
+bin_edges = [0,10,20,30,40,50,60,70,80,90,100]
+
+plt.hist(ages, bins = 5, edgecolor ='black')
+plt.show()
 
 
+#x=years
+#y= money
+'''
 def compute_histogram(self, image):
     """Computes the histogram of the input image
     takes as input:
@@ -20,3 +35,8 @@ def compute_histogram(self, image):
             hist[int(image[x, y])] += 1
 
     return hist
+    '''
+
+# Make calculations for Charts.js
+# Total Debt = (Loan Amount * Interest Rate) + Loan Amount
+# How much does a student need to pay monthly? (the amount that covers the interest and principle)
